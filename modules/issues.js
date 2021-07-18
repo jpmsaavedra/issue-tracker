33636f3aea57a36d3ff000730bba4f1e9a40d560
Joao Saavedra <monteir4@coventry.ac.uk>
2021-07-18 10:38:26 +0000

    Modified issues module
    fixed linting errors and some bugs

--- modules/issues.js
+++ modules/issues.js
@@ -17,7 +17,9 @@ class Issues {
 			this.db = await sqlite.open(dbName)
 			// we need this table to store the issues
 			const sql = 'CREATE TABLE IF NOT EXISTS issues\
-				(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, location_desc TEXT, detailed_desc TEXT, date DEFAULT CURRENT_TIMESTAMP, status TEXT, photo TEXT, reporter INTEGER, FOREIGN KEY(reporter) REFERENCES user(id));'
+				(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, \
+                locationDesc TEXT, detailedDesc TEXT, date DEFAULT CURRENT_TIMESTAMP, \
+                status TEXT, photo TEXT, reporter INTEGER, FOREIGN KEY(reporter) REFERENCES user(id));'
 			await this.db.run(sql)
 			return this
 		})()
@@ -31,12 +33,17 @@ class Issues {
 	 * @param {String} photo the path of the issue picture
 	 * @param {String} user the id of the user who submitted the issue
 	 */
-	async submit(name, location_desc, detailed_desc, photo, user) {        
-        let sql = `SELECT id AS id FROM users WHERE user="${user}";`
-        const record = await this.db.get(sql)
-        const reporter = record.id
-        const status = 'new';
-        sql = `INSERT INTO issues(name, location_desc, detailed_desc, status, photo, reporter) VALUES("${name}", "${location_desc}", "${detailed_desc}", "${status}", "${photo}", "${reporter}")`
+	async submit(name, locationDesc, detailedDesc, photo, user) {
+		let sql = `SELECT id AS id FROM users WHERE user="${user}";`
+		const record = await this.db.get(sql)
+		const reporter = record.id
+		const status = 'new'
+		if(!name) throw new Error('issue must have a name')
+		if(!locationDesc) throw new Error('issue must have a location description')
+		const minChar = 100
+		if(detailedDesc.length < minChar) throw new Error('issue description must be longer than 100 characters')
+		sql = `INSERT INTO issues(name, location_desc, detailed_desc, status, photo, reporter) VALUES("${name}", \
+            "${locationDesc}", "${detailedDesc}", "${status}", "${photo}", "${reporter}")`
 		await this.db.run(sql)
 		return true
 	}
