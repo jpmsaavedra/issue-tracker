84d01fdfaa8d7ec239ea1a1074f0c78bfde6bd38
Joao Saavedra <monteir4@coventry.ac.uk>
2021-07-19 03:04:48 +0000

    Modified main javascript script
    added markdown converter to the dom listener

--- public/main.js
+++ public/main.js
@@ -10,4 +10,15 @@ window.addEventListener('DOMContentLoaded', () => {
 			document.querySelector('aside').hidden = true
 		},delay)
 	}
+    let converter = new showdown.Converter({'tables': true, 'tasklists': true, 'strikethrough': true})
+    const options = converter.getOptions()
+    console.log(options)
+	document.querySelector('textarea').addEventListener('input', event => {
+        console.log('change')
+        const markdown = document.querySelector('textarea').value
+        console.log(markdown)
+		const html = converter.makeHtml(markdown)
+		console.log(html)
+		document.querySelector('article').innerHTML = html
+	})
 })
